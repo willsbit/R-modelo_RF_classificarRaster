0f740e3773d58e67975fa84f5fd6d07574138d97
Willian <60491870+willsbit@users.noreply.github.com>
2021-06-29 16:13:07 -0300

    Update modelo_RF.r

--- modelo_RF.r
+++ modelo_RF.r
@@ -1,3 +1,5 @@
+# Modelo RF simples para a classificação de um raster a partir de informações extraidas de pontos amostrais coletados.
+
 # instalando as bibliotecas:
 install.packages("randomForest")
 install.packages("sf")
@@ -14,26 +16,26 @@ library(tiff)
 
 # Carregando os dados: ----
 # raster
-setwd("E:/estagio_e/classificacaoR_arquivos/tifs_wgs")
+setwd("C:/meu_diretorio/tif_wgs")
 img <- stack(list.files(".", pattern = ".tif$", full.names = T))
 
-plotRGB(img, 4, 2, 1, stretch = 'hist') # blue=1, green=2, nir=3, red=4
+plotRGB(img, 4, 2, 1, stretch = 'hist') #para uma imagem pleiades, blue=1, green=2, nir=3, red=4
 names(img) <- paste0(rep('band', nlayers(img)), 1:nlayers(img))
 
 
 # amostras
-setwd("E:/estagio_e/classificacaoR_arquivos/shapes_wgs")
-tb <- read_sf("pontos_TBWGS.shp")
-tfc <-read_sf("pontos_TFCWGS.shp")
-tzn <-read_sf("pontos_TZnWGS.shp")
-naotelhado <-read_sf("pontos_naoTelhadoWGS.shp")
-amostras<-rbind(tb, tfc, tzn, naotelhado)
+setwd("C:/meu_diretorio/shapes_wgs")
+c1 <- read_sf("classe1.shp")
+c2 <-read_sf("classe2.shp")
+c3 <-read_sf("classe3.shp")
+c4 <-read_sf("classe4.shp")
+amostras<-rbind(c1, c2, c3, c4)
 plot(amostras, add=T)
 x <- c(1:20000)  
 amostras<-cbind(amostras,x)
 
 # Amostras de treino de teste ----
-teste <- amostras %>% group_by(classe) %>% sample_n(size = 1500 ) 
+teste <- amostras %>% group_by(classe) %>% sample_n(size = 1500 ) #note que "classe" é um atributo de cada entrada do conjunto de dados
 #1500 amostras de teste por classe
 treino <- amostras %>% filter(! x %in% (teste$x))
 nrow(treino)
@@ -62,7 +64,7 @@ class(valsTrain$class)
 rf.mdl <- randomForest(valsTrain$class ~., data = valsTrain, xtest = subset(valsTest, select = -class), ytest = valsTest$class, keep.forest = TRUE)
 
 #confusionMatrix da fase de treinamento
-setwd("E:/estagio_e/classificacaoR_arquivos")
+setwd("C:/meu_diretorio")
 rf.mdl$confusion 
 
 options(max.print = 6000)
